91c3a7b55507941d40c5092e8620ddb1487b5267
Manva <pradhanmanva@gmail.com>
2017-03-31 23:37:27 +0530

    created findPlayer()

--- src/AIGame_Isola.java
+++ src/AIGame_Isola.java
@@ -1,6 +1,32 @@
 /**
  * Created by pradh on 3/31/2017.
  */
-public class AIGame_Isola  {
-    //class
+public class AIGame_Isola {
+    int N = 7;
+    int[][] Board = new int[N][N];
+
+    //Constructor
+    AIGame_Isola() {
+        System.out.println("");
+    }
+
+    //FindPlayer() - finds the player position and returns in the Point Object
+    Point findPlayer(int id) {
+        Point position = new Point();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (id == Board[i][j]) {
+                    position.setX = i;
+                    position.setY = j;
+                }
+            }
+        }
+        return position;
+    }
+
+    //FindBlocked - find the number of blocked states around that point
+    int findBlocked(Point position) {
+        return
+    }
 }
+
