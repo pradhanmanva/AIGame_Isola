37ef34864322669b4672587f9f9bda4e992eeb63
Gaurav <gj100596@gmail.com>
2017-03-31 23:35:12 +0530

    Pont Class

---
+++ src/Point.java
@@ -0,0 +1,18 @@
+
+public class Point {
+	int x,y;
+	
+	void setX(int x){
+		this.x = x; 
+	}
+	void setY(int y){
+		this.y = y; 
+	}
+	
+	int getX(){
+		return x;
+	}
+	int getY(){
+		return y;
+	}
+}
